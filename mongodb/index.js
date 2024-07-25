a312e319972ac8827ad033b0895f43be76d2c66a
930-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-07-25 09:19:09 +0530

    updates

--- mongodb/index.js
+++ mongodb/index.js
@@ -12,7 +12,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: "EcoChat",
+      dbName: "public_chat",
     });
 
     isConnected = true;
@@ -22,3 +22,6 @@ export const connectToDB = async () => {
     console.log(error);
   }
 };
+
+
+
